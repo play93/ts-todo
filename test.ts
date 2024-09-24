06a6311f4f8a8ff0f73c9bc6f0c1d448918f55f9
play93 <95739833+play93@users.noreply.github.com>
2024-09-24 18:36:39 +0900

    Project Start

---
+++ test.ts
@@ -0,0 +1,14 @@
+type Todo = {
+  id: string;
+  title: string;
+  completed: boolean;
+};
+
+export async function getTodos() {
+  const res = await fetch("http://localhost:4000/todos");
+  const data: Todo[] = await res.json();
+
+  return data;
+}
+
+getTodos().then(console.log);
